a414d60e230e3ef4a3764a79c18d05bdb460b0da
Adrion10 <lacehila@gmail.com>
2022-06-27 22:25:32 +0200

    string

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function add(n1, n2) {
     return n1 + n2;
 }
-var number1 = 5;
+// const number1 = 5;// number
+var number1 = "5";
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
