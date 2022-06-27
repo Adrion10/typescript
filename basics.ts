d0481d8a8354581c3ac69e8c01f6f05c63bcd531
Adrion10 <lacehila@gmail.com>
2022-06-27 23:10:20 +0200

    object

---
+++ basics.ts
@@ -0,0 +1,21 @@
+function add(n1: number, n2: number, showresult: boolean, phrase: string) {
+  //   console.log(typeof number1);
+  //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //     throw new Error("Incorrect Input!");
+  //   }
+  const result = n1 + n2;
+  if (showresult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+let number1 = 5; // number
+//const number1 = "5"; // string
+const number2 = 2.8;
+
+const printResult = true;
+const resultPhrase = "Result Is: ";
+
+add(number1, number2, printResult, resultPhrase);
