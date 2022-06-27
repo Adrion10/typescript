7b9ddce3651f099709e963e121fb576ac31908ef
Adrion10 <lacehila@gmail.com>
2022-06-27 22:53:21 +0200

    number1, number2, printResult, resultPhrase

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-function add(n1, n2) {
+function add(n1, n2, showresult, phrase) {
     //   console.log(typeof number1);
-    if (typeof n1 !== "number" || typeof n2 !== "number") {
-        throw new Error("Incorrect Input!");
+    //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Incorrect Input!");
+    //   }
+    var result = n1 + n2;
+    if (showresult) {
+        console.log(phrase + result);
     }
-    return n1 + n2;
+    else {
+        return result;
+    }
 }
-//const number1 = 5; // number
-var number1 = "5"; // string
+var number1 = 5; // number
+//const number1 = "5"; // string
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result Is: ";
+add(number1, number2, printResult, resultPhrase);
