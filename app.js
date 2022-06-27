bb735b6eb54890bc44c297b1153aa3aee08df331
Adrion10 <lacehila@gmail.com>
2022-06-27 22:41:29 +0200

    chek typeof

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function add(n1, n2) {
-    console.log(typeof number1);
+    //   console.log(typeof number1);
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+        throw new Error("Incorrect Input!");
+    }
     return n1 + n2;
 }
-var number1 = 5; // number
-// const number1 = "5"; // string
+//const number1 = 5; // number
+var number1 = "5"; // string
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
