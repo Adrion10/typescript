8cd29d2265de873761586f6aeffa8276a7472cd3
Adrion10 <lacehila@gmail.com>
2022-06-27 23:16:49 +0200

    array

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 // } = {
 var person = {
     name: "Lace",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+// let favoriteActivites: string[];
+var favoriteActivites;
+favoriteActivites = ["Sports", 1];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toLocaleUpperCase());
+}
--- app.ts
+++ app.ts
@@ -5,5 +5,14 @@
 const person = {
   name: "Lace",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
+
+// let favoriteActivites: string[];
+let favoriteActivites: any[];
+favoriteActivites = ["Sports", 1];
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase());
+}
