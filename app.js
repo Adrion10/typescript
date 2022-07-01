e8edd2093a5f78521ca29d325a16590007f4df68
Adrion10 <lacehila@gmail.com>
2022-07-01 20:36:48 +0200

    literal concept

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 function combine(input1, input2, resultConversation) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversation === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    if (resultConversation === "as-number") {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+    return result;
+    // if (resultConversation === "as-number") {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
 }
 var combinedAges = combine(30, 40, "as-number");
 console.log(combinedAges);
-var combinedName = combine("Lace", "Noel", "as-text");
-console.log(combinedName);
 var combinedStringAges = combine("30", "40", "as-number");
 console.log(combinedStringAges);
+var combinedName = combine("Lace", "Noel", "as-text");
+console.log(combinedName);
--- app.ts
+++ app.ts
@@ -4,23 +4,27 @@ function combine(
   resultConversation: string
 ) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversation === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultConversation === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+  return result;
+  // if (resultConversation === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
 const combinedAges = combine(30, 40, "as-number");
 console.log(combinedAges);
 
-const combinedName = combine("Lace", "Noel", "as-text");
-console.log(combinedName);
-
 const combinedStringAges = combine("30", "40", "as-number");
 console.log(combinedStringAges);
+
+const combinedName = combine("Lace", "Noel", "as-text");
+console.log(combinedName);
