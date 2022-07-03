514e8c28fbee7317b66e380a200a8511e33b755f
Adrion10 <lacehila@gmail.com>
2022-07-03 18:08:33 +0200

    combineValue

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ function add(n1, n2) {
     return n1 + n2;
 }
 function printResult(num) {
+    //undefined
     console.log("Result: " + num);
 }
 printResult(add(5, 17));
+console.log(printResult(add(5, 17))); // undefined
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+var combineValue;
+combineValue = add;
+console.log(combineValue(4, 6));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
